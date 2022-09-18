c91fe644bf1e822e91a736e09c9fa8e8e176e047
aridsm <aniemorelanie@gmail.com>
2022-09-18 17:26:42 -0300

    add

--- src/components/layout/produtos/ProdutoItem.js
+++ src/components/layout/produtos/ProdutoItem.js
@@ -1,10 +1,9 @@
-import React, { useContext, useRef, useState } from 'react'
-import { CartContext } from '../../../context/CartContext';
-import WrapperButton from '../../utilities/WrapperButton'
-import classes from './ProdutoItem.module.css'
+import React, { useContext, useRef, useState } from "react";
+import { CartContext } from "../../../context/CartContext";
+import WrapperButton from "../../utilities/WrapperButton";
+import classes from "./ProdutoItem.module.css";
 
 const ProdutoItem = ({ nome, img, preco }) => {
-
   const [quantidade, setQuantidade] = useState(1);
   const [isAlertShown, setIsAlertShown] = useState(false);
   const animaTimeout = useRef();
@@ -12,45 +11,60 @@ const ProdutoItem = ({ nome, img, preco }) => {
   const { addItem } = useContext(CartContext);
 
   const addItemToCartHandler = () => {
-
-    clearTimeout(animaTimeout.current)
+    clearTimeout(animaTimeout.current);
 
     addItem({
       nome: nome,
       preco: preco,
       quantidade: +quantidade,
-      img: img
-    })
+      img: img,
+    });
 
     setIsAlertShown(true);
     const animationDuration = 1000;
 
     animaTimeout.current = setTimeout(() => {
       setIsAlertShown(false);
-      console.log('1')
-    }, animationDuration)
-  }
+      console.log("1");
+    }, animationDuration);
+  };
 
   return (
     <li className={classes.card}>
-      <div><img src={require(`../../../assets/imgs-produtos/${img}`)} alt={nome} /></div>
+      <div>
+        <img src={require(`../../../assets/imgs-produtos/${img}`)} alt={nome} />
+      </div>
       <div className={classes.infos}>
-        <p className={classes.nome}>{nome}</p>
-        <p>
-          <span className={classes.reais}>R$</span>
-          <span className={classes.preco}>{preco}</span>
-        </p>
+        <div className={classes.precoENome}>
+          <p className={classes.nome}>{nome}</p>
+          <p>
+            <span className={classes.reais}>R$</span>
+            <span className={classes.preco}>{preco}</span>
+          </p>
+        </div>
         <form className={classes.quantidadeForm}>
-          <label>qt. <input type='number' name='quantidade' id='quantidade' value={quantidade} onChange={(e) => setQuantidade(e.target.value)} step='1' min='1' /></label>
+          <label>
+            qt.
+            <input
+              type="number"
+              name="quantidade"
+              id="quantidade"
+              value={quantidade}
+              onChange={(e) => setQuantidade(e.target.value)}
+              step="1"
+              min="1"
+            />
+          </label>
         </form>
         <div className={classes.btnAlert}>
           {isAlertShown && <span className={classes.alertAdd}>+</span>}
-          <WrapperButton className={classes.btn} onClick={addItemToCartHandler}>Adicionar ao carrinho</WrapperButton>
+          <WrapperButton className={classes.btn} onClick={addItemToCartHandler}>
+            Adicionar ao carrinho
+          </WrapperButton>
         </div>
-
       </div>
     </li>
-  )
-}
+  );
+};
 
-export default ProdutoItem
+export default ProdutoItem;
