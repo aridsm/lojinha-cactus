8184da45ebf1bb8ad7ca9f9292dc256177b1471f
aridsm <aniemorelanie@gmail.com>
2022-11-29 18:59:38 -0300

    new plants added

--- src/lista_produtos.js
+++ src/lista_produtos.js
@@ -17,6 +17,15 @@ export const all_products = [
       "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
   },
   {
+    nome: "Neque Volutpat",
+    preco: "39.99",
+    img: "img40.webp",
+    categoria: "cactos",
+    cor: "verde",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
+  },
+  {
     nome: "Ligula ipsum",
     preco: "22.25",
     img: "img3.webp",
@@ -52,11 +61,11 @@ export const all_products = [
       "Aliquam accumsan mollis quam sed dictum. Duis eget malesuada nisi.",
   },
   {
-    nome: "Curabitur",
-    preco: "12.25",
-    img: "img7.webp",
-    categoria: "outros",
-    cor: "verde",
+    nome: "Ultrices",
+    preco: "38.95",
+    img: "img48.webp",
+    categoria: "flores",
+    cor: "amarelo",
     description:
       "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
   },
@@ -78,6 +87,15 @@ export const all_products = [
       "Aliquam accumsan mollis quam sed dictum. Duis eget malesuada nisi.",
   },
   {
+    nome: "Mauris",
+    preco: "49.99",
+    img: "img50.webp",
+    categoria: "flores",
+    cor: "amarelo",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
+  },
+  {
     nome: "Cras sit",
     preco: "75.10",
     img: "img10.webp",
@@ -93,6 +111,15 @@ export const all_products = [
     categoria: "cactos",
     description: "Cras quis nunc consectetur ex tristique posuere.",
     cor: "verde",
+  },
+  {
+    nome: "Bibendum",
+    preco: "33.59",
+    img: "img47.webp",
+    categoria: "flores",
+    cor: "amarelo",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
   },
   {
     nome: "Ullamcorper augue",
@@ -241,6 +268,15 @@ export const all_products = [
       "Aliquam accumsan mollis quam sed dictum. Duis eget malesuada nisi.",
   },
   {
+    nome: "Lacus Rhoncus",
+    preco: "25.85",
+    img: "img42.webp",
+    categoria: "cactos",
+    cor: "verde",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
+  },
+  {
     nome: "Lacinia finibus",
     preco: "67.75",
     img: "img29.webp",
@@ -267,6 +303,15 @@ export const all_products = [
       "Aliquam accumsan mollis quam sed dictum. Duis eget malesuada nisi.",
   },
   {
+    nome: "Efficitur Justo",
+    preco: "25.15",
+    img: "img41.webp",
+    categoria: "cactos",
+    cor: "verde",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
+  },
+  {
     nome: "Ultricies",
     preco: "36.49",
     img: "img32.webp",
@@ -291,6 +336,15 @@ export const all_products = [
     cor: "rosa",
     description:
       "Aliquam accumsan mollis quam sed dictum. Duis eget malesuada nisi.",
+  },
+  {
+    nome: "Nibh Eu",
+    preco: "28.59",
+    img: "img51.webp",
+    categoria: "flores",
+    cor: "roxo",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
   },
   {
     nome: "Tincidunt ",
@@ -332,6 +386,123 @@ export const all_products = [
     img: "img39.webp",
     categoria: "flores",
     cor: "laranja",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
+  },
+  {
+    nome: "Ante Posuere",
+    preco: "22.99",
+    img: "img43.webp",
+    categoria: "cactos",
+    cor: "verde",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
+  },
+  {
+    nome: "In Convallis Ipsum",
+    preco: "39.49",
+    img: "img46.webp",
+    categoria: "flores",
+    cor: "branco",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
+  },
+  {
+    nome: "Sapien",
+    preco: "79.99",
+    img: "img49.webp",
+    categoria: "flores",
+    cor: "amarelo",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
+  },
+  {
+    nome: "Malesuada",
+    preco: "35.49",
+    img: "img52.webp",
+    categoria: "flores",
+    cor: "roxo",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
+  },
+  {
+    nome: "Adipiscing",
+    preco: "72.25",
+    img: "img53.webp",
+    categoria: "flores",
+    cor: "rosa",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
+  },
+  {
+    nome: "Hendrerit",
+    preco: "41.75",
+    img: "img54.webp",
+    categoria: "flores",
+    cor: "laranja",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
+  },
+  {
+    nome: "Imperdiet",
+    preco: "56.49",
+    img: "img55.webp",
+    categoria: "flores",
+    cor: "rosa",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
+  },
+  {
+    nome: "Tempus Lectus",
+    preco: "17.90",
+    img: "img56.webp",
+    categoria: "flores",
+    cor: "roxo",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
+  },
+  {
+    nome: "Curabitur",
+    preco: "12.25",
+    img: "img7.webp",
+    categoria: "outros",
+    cor: "verde",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
+  },
+  {
+    nome: "Pulvinar Risus",
+    preco: "19.99",
+    img: "img45.webp",
+    categoria: "cactos",
+    cor: "verde",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
+  },
+  {
+    nome: "Leo",
+    preco: "28.99",
+    img: "img57.webp",
+    categoria: "flores",
+    cor: "rosa",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
+  },
+  {
+    nome: "Enim",
+    preco: "31.79",
+    img: "img58.webp",
+    categoria: "flores",
+    cor: "rosa",
+    description:
+      "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
+  },
+  {
+    nome: "Sem Cursus",
+    preco: "29.99",
+    img: "img44.webp",
+    categoria: "cactos",
+    cor: "verde",
     description:
       "Vestibulum eget placerat mauris. Ut quis commodo tortor, a iaculis metus.",
   },
